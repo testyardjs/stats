e6d9d49b6d3feffaca726f287ac49084b45ec860
Richard King <richrdkng@gmail.com>
2023-07-16 10:02:17 +0200

    feat: add default stats iteration length

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,10 @@
+/**
+ * Default iteration length for gathering stats data.
+ *
+ * @default 1000000
+ */
+export const LENGTH = 1_000_000
+
 export { default as BooleanStats } from './BooleanStats'
 export { default as FloatStats } from './FloatStats'
 export { default as IntegerStats } from './IntegerStats'
